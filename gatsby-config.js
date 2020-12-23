ccaa9a0c8bf8752c5bffdbd9b1ec4f595f2d7a02
Daniil Chistyakov <4i.danila@gmail.com>
2020-12-23 20:14:29 +0300

    Bugs fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/images/gatsby-icon.png`,
+        icon: `content/images/icon.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
