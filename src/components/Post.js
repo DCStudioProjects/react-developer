4905e5c91a167fe57a34fe505c6ab451a57e1e42
Daniil Chistyakov <4i.danila@gmail.com>
2020-12-23 18:49:30 +0300

    Developing 23rd December 18:49

---
+++ src/components/Post.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const Post = ({ post }) => (
+  <article className="card ">
+    <header>
+    </header>
+  </article>
+)
+export default Post
